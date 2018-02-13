41ef5e7ea633526128bf54d4368d68067ed80555
Otto <o.stegmaier@gmail.com>
2018-02-12 20:12:57 -0800

    Replace clarifai api with request

--- js/index.js
+++ js/index.js
@@ -5,11 +5,11 @@ var CVA__ = {};
 
 CVA__.watchsubmit = function () {
   console.log('Im ready!');
-  const form = $('.search-box');
-  const urlInput = $('#image-url');
-  form.submit(e => {
+  const $form = $('.search-box');
+  const $urlInput = $('#image-url');
+  $form.submit(e => {
     e.preventDefault();
-    const url =  urlInput.val();
+    const url =  $urlInput.val();
     const urlOk = CVA__.validateURL(url);
     if (urlOk) {
       CVA__.classifyImage(url);
@@ -26,13 +26,25 @@ CVA__.classifyImage = function (url) {
   const data = {
     imageUrl: url,
   };
+  CVA__.clarifaiRequest(data);
+  // CVA__.googleRequest(data);
+};
 
+CVA__.googleRequest = function (data) {
   $.post('/google', data, function (response) {
-    console.log(response);
+    console.log('Sending image for classification to Google Vision.');
+    const $container = $('.google-results');
+    $container.html(`<pre>${JSON.stringify(response, null, 2)}</pre>`);
+    $container.prop('hidden', false);
   });
+};
 
+CVA__.clarifaiRequest = function (data) {
   $.post('/clarifai', data, function (response) {
-    console.log(response);
+    console.log('Sending image for classification to clarifai.');
+    const $container = $('.clarifai-results');
+    $container.html(`<pre>${JSON.stringify(response, null, 2)}</pre>`);
+    $container.prop('hidden', false);
   });
 };
 
--- main.py
+++ main.py
@@ -18,12 +18,22 @@ JINJA_ENVIRONMENT = jinja2.Environment(
 
 def make_google_data(url):
   """Constructs the data object for a post request to Google Vision API."""
+  headers = {'Content-Type': 'application/json'}
   image_data = {
       "image": {"source": {"imageUri": url}},
       "features": [{"type": "LABEL_DETECTION"}]
   }
   data = {"requests": [image_data]}
-  return json.dumps(data)
+  return headers, json.dumps(data)
+
+def make_clarifai_data(url, auth_key):
+  """Constructs the data object for a post request to Clarifai."""
+  headers = {
+      'Content-Type': 'application/json',
+      'Authorization': 'Key {}'.format(auth_key)
+  }
+  data = {"inputs": [{"data": {"image": {"url": url}}}]}
+  return headers, json.dumps(data)
 
 
 class Settings(ndb.Model):
@@ -43,8 +53,8 @@ class Settings(ndb.Model):
       retval.put()
     if retval.value == not_set_value:
       raise Exception(
-          'Setting %s not found in the database.' %(name) + 
-          'Use the app engine developer console and add a value.' 
+          'Setting %s not found in the database.' %(name) +
+          'Use the app engine developer console and add a value.'
       )
     return retval.value
 
@@ -62,16 +72,14 @@ class ClarifaiWrapper(webapp2.RequestHandler):
   """Wrapper class to send post request to Clarifai vision api."""
 
   def post(self):
-    """Responds to post request with clarifai prediction/"""
     clarifai_key = Settings.get('CLARIFAI_API_KEY')
-    clarifai_app = ClarifaiApp(api_key=clarifai_key)
-    model = clarifai_app.models.get("general-v1.3")
     image_url = self.request.get('imageUrl')
-    prediction = model.predict_by_url(url=image_url)
-    logging.info('Prediction from clarifai app: %s', prediction)
-    self.response.headers['Content-Type'] = 'text/plain'
-    self.response.write(prediction)
-
+    headers, data = make_clarifai_data(image_url, clarifai_key)
+    url = 'https://api.clarifai.com/v2/models/aaa03c23b3724a16a56b629203edc62c/outputs'
+    response = requests.post(url, data=data, headers=headers)
+    logging.info('Prediction from clarifai: %s', response.content)
+    self.response.headers['Content-Type'] = 'text/json'
+    self.response.write(response.content)
 
 class GoogleWrapper(webapp2.RequestHandler):
   """Wrapper class to send an image classification request to Google."""
@@ -79,14 +87,12 @@ class GoogleWrapper(webapp2.RequestHandler):
   def post(self):
     """Responds to post request with Google Vision API prediction"""
     google_key = Settings.get('GOOGLE_API_KEY')
-    headers = {
-        'Content-Type': 'application/json',
-    }
     image_url = self.request.get('imageUrl')
-    data = make_google_data(image_url)
+    headers, data = make_google_data(image_url)
     url = 'https://vision.googleapis.com/v1/images:annotate?key=' + google_key
     response = requests.post(url, data=data, headers=headers)
-    self.response.headers['Content-Type'] = 'text/plain'
+    logging.info('Prediction from google vision: %s', response.content)
+    self.response.headers['Content-Type'] = 'text/json'
     self.response.write(response.content)
 
 app = webapp2.WSGIApplication([
