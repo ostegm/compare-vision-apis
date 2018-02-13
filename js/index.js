3469938fa92e3d1ff5e7fc527d441782dd0c8a1e
Otto <o.stegmaier@gmail.com>
2018-02-12 21:01:06 -0800

    Display data from both api's

--- js/index.js
+++ js/index.js
@@ -13,8 +13,15 @@ CVA__.watchsubmit = function () {
     const urlOk = CVA__.validateURL(url);
     if (urlOk) {
       CVA__.classifyImage(url);
+      CVA__.showImage(url);
     }
   });
+};
+
+CVA__.showImage = function (url) {
+  $('.results').prop('hidden', false);
+  $img = $('.result-image');
+  $img.prop('src', url).prop('alt', 'An image input by the user.');
 };
 
 CVA__.validateURL = function (url) {
@@ -27,25 +34,36 @@ CVA__.classifyImage = function (url) {
     imageUrl: url,
   };
   CVA__.clarifaiRequest(data);
-  // CVA__.googleRequest(data);
+  CVA__.googleRequest(data);
 };
 
 CVA__.googleRequest = function (data) {
   $.post('/google', data, function (response) {
     console.log('Sending image for classification to Google Vision.');
-    const $container = $('.google-results');
-    $container.html(`<pre>${JSON.stringify(response, null, 2)}</pre>`);
-    $container.prop('hidden', false);
+    const $container = $('.google');
+    const labelObjects = response.responses[0].labelAnnotations;
+    CVA__.parseResponse(labelObjects, 'description', 'score', $container);
   });
 };
 
 CVA__.clarifaiRequest = function (data) {
   $.post('/clarifai', data, function (response) {
     console.log('Sending image for classification to clarifai.');
-    const $container = $('.clarifai-results');
-    $container.html(`<pre>${JSON.stringify(response, null, 2)}</pre>`);
-    $container.prop('hidden', false);
+    const $container = $('.clarifai');
+    const labelObjects = response.outputs[0].data.concepts;
+    CVA__.parseResponse(labelObjects, 'name', 'value', $container);
   });
+};
+
+CVA__.parseResponse = function (labelObjects, labelFieldName, scoreFieldName, $container) {
+  labelsList = labelObjects.map(function (i) {
+    return {
+      label: i[labelFieldName],
+      score: i[scoreFieldName],
+    };
+  });
+
+  $container.append(`<pre>${JSON.stringify(labelsList, null, 2)}</pre>`);
 };
 
 $(CVA__.watchsubmit);
